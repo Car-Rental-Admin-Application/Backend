a07a9745d6165d76f496fe8ec3f03f07425e352f
psyphonz <zaaitwassim@gmail.com>
2025-06-20 11:06:17 +0100

    fix dependencies

--- backend/vehicle-service/src/vehicle/vehicle.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class VehicleResolver {
-  @Query(() => String)
-  hello() {
-    return 'Vehicle Service is working!';
-  }
-}
