20359069d7ef65400b5a28cc6d783296959a9706
psyphonz <zaaitwassim@gmail.com>
2025-06-20 16:15:53 +0100

    gitignore

--- frontend/src/app/admin/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-// Import your admin components here
-// import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-
-export default [
-  // Example route:
-  // { path: '', component: AdminDashboardComponent }
-] as Routes;
--- frontend/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { jwtInterceptor } from './jwt.interceptor';
-
-export const appConfig = {
-  providers: [
-    provideHttpClient(withInterceptors([jwtInterceptor])) // Add this
-  ]
-};
